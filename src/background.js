86b138e7877e6019611ac1b18e09d22ca447a2f3
Philipp <Philipp.gramig@gmail.com>
2020-04-28 21:41:58 +0200

    Removed comments

--- src/background.js
+++ src/background.js
@@ -2,23 +2,16 @@
 import {
   app, protocol, BrowserWindow, Tray
 } from "electron";
-import {
-  createProtocol
-  /* installVueDevtools */
-} from "vue-cli-plugin-electron-builder/lib";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import path from "path";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win;
 
-// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([{ scheme: "app", privileges: { secure: true, standard: true } }]);
 
 function createWindow() {
-  // Create the browser window.
   win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -28,19 +21,14 @@ function createWindow() {
   });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
     if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
     createProtocol("app");
-    // Load the index.html when not in development
     win.loadURL("app://./index.html");
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
   const iconPath = path.join("src", "assets", "trayIcon.png");
   const tray = new Tray(iconPath);
@@ -50,7 +38,6 @@ app.on("ready", async () => {
   });
 });
 
-// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === "win32") {
     process.on("message", (data) => {

11ae3f1668f5bed00d91eb4d090025a015754af3
Philipp <Philipp.gramig@gmail.com>
2020-04-28 21:39:40 +0200

    Removed code out of background.js

--- src/background.js
+++ src/background.js
@@ -36,28 +36,7 @@ function createWindow() {
     // Load the index.html when not in development
     win.loadURL("app://./index.html");
   }
-
-  win.on("closed", () => {
-    win = null;
-  });
 }
-
-// Quit when all windows are closed.
-app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
-});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
